8b2f3dae3f627b322ad239fc43209235e0128075
joe <goldglovejo13@gmail.com>
2021-01-16 10:14:54 -0600

    Add project files.

---
+++ MaskedInformation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MaskedInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Psst Hey! Tell me a secret, promise I won't tell");
+            var secret = Console.ReadLine();
+            var maskedSecret = secret.Substring(secret.Length - 4).PadLeft(secret.Length, '*');
+
+            Console.WriteLine(maskedSecret);
+        }
+    }
+}
